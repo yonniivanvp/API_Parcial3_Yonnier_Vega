7908cc5ef1a22062e4793412ec6f374b85696c03
Yonnier Ivan Vega Palacios <yonnivanvp@hotmail.com>
2023-10-28 15:31:29 -0500

    Create the AuditBase class

---
+++ API_Parcial3/DAL/Entities/AuditBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Parcial3.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key]
+        [Required] 
+        public virtual Guid Id { get; set; }
+        public virtual DateTime? CreatedDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+    }
+}
